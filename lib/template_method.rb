4e19a426846f9ae5fccf571addbfc58cd92f04c4
Rajesh Pillai <pillai.rajesh@gmail.com>
2015-12-18 23:17:15 +0530

    Create separate class for actors

--- lib/template_method.rb
+++ lib/template_method.rb
@@ -1,17 +1,35 @@
 class Hero
   attr_reader :damage, :abilities
 
-  def initialize (occupation = nil)
-    if occupation == :warrior
-      @damage = 15
-      @abilities = [:strike]
-    elsif occupation == :mage
-        @damage = 7
-        @abilities = [:magic_arrow]
-    else
-      @damage = 10
-      @abilities = []
-    end
+  def initialize 
+    @damage = 10
+    @abilities = []
+  end
+
+  def attack
+    "Attacked dealing #{damage} damage"
+  end
+end
+
+class Warrior
+  attr_reader :damage, :abilities
+
+  def initialize 
+    @damage = 15
+    @abilities = [:strike]
+  end
+
+  def attack
+    "Attacked dealing #{damage} damage"
+  end
+end
+
+class Mage
+  attr_reader :damage, :abilities
+
+  def initialize 
+    @damage = 7
+    @abilities = [:magic_arrow]
   end
 
   def attack
