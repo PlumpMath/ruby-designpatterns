d7e77b8ca1ab4cf059704782791b08c93513ab3e
Rajesh Pillai <pillai.rajesh@gmail.com>
2015-12-18 23:08:17 +0530

    Introduce occupation for Hero: Warrior

--- spec/template_method_spec.rb
+++ spec/template_method_spec.rb
@@ -2,12 +2,23 @@ require 'spec_helper'
 require_relative '../lib/template_method'
 
 describe "Template Method Pattern" do 
+
+  context "Default Hero" do
     let(:hero) { Hero.new }
     it "has default damage rating of 10" do
-        expect(hero.damage).to eq(10)
+      expect(hero.damage).to eq(10)
     end
 
     it "can attack" do
-        expect(hero.attack).to eq("Attacked dealing 10 damage")
+      expect(hero.attack).to eq("Attacked dealing 10 damage")
     end
+  end
+
+  context "Warrior" do
+    let(:hero) { Hero.new :warrior}
+    it "has default damage rating of 15" do
+      expect(hero.damage).to eq(15)
+    end
+  end
+
 end
